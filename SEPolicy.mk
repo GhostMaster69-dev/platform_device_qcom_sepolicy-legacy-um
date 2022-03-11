896c045d510c48a18ac8c0293cd64292543fbe3e
GhostMaster69-dev <rathore6375@gmail.com>
2022-03-11 22:21:48 +0530

    sepolicy: Include SyberiaOS specific QCOM sepolicy
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -47,3 +47,5 @@ ifneq (,$(filter sdm845 sdm710 sdm660 msm8937 msm8953 msm8996 msm8998, $(TARGET_
     BOARD_VENDOR_SEPOLICY_DIRS += $(SEPOLICY_PATH)/legacy/vendor/test
     endif
 endif
+
+-include device/syberia/sepolicy/qcom/sepolicy.mk
